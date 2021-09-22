d315b9f5caa7fdff926b0298c836cb30ec58205b
Kenny Fournie <FourKenn744@GoTVDSB.ca>
2021-09-22 13:55:39 -0400

    made basic stages (possibly needs changes?)

--- Program.cs
+++ Program.cs
@@ -10,6 +10,46 @@ namespace part_5_console_decisions
     {
         static void Main(string[] args)
         {
+            //stages
+            
+            int age;
+            string result;
+
+            Console.WriteLine("Please enter your age");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            if (age > 0 && age <= 5)
+            {
+                result = "toddler";
+                Console.WriteLine($"you are a " + result);
+            }
+
+            else if (age > 5 && age <= 10)
+            {
+                result = "child";
+                Console.WriteLine($"you are a " + result);
+            }
+
+            else if (age > 10 && age <= 12)
+            {
+                result = "preteen";
+                Console.WriteLine($"you are a " + result);
+            }
+
+            else if (age > 12 && age <= 18)
+            {
+                result = "teen";
+                Console.WriteLine($"you are a " + result);
+            }
+
+
+            else if (age > 18)
+            {
+                result = "adult";
+                Console.WriteLine($"you are a " + result);
+            }
+
+            Console.ReadLine();
         }
     }
 }
